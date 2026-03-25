e262c89ad86c7516d750cc71268177a7d018d98c
unknown <shreekantalakshmi95@gmail.com>
2026-03-26 04:44:34 +0530

    Updated control structures

---
+++ ControlStructures.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+//23public class ControlStructure {
+    //public static void main(String[] args){
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the age");
+        // int age = sc.nextInt();
+        // if(age>=18)
+        //     System.out.println("Eligible for voting");
+        // sc.close();
+        //if else biggest of two numbers
+    public class ControlStructures{
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+        if (a>b)
+            System.out.println("First number is larger");
+        else
+            System.out.println("Second number is bigger");
+
+
+        }
+    }
+        
+    
+
+
+
+    
+
+    
+
